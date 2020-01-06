a474114b1be776bce9de5e47153dcb3180148103
xoreo <mattnappo@gmail.com>
2020-01-05 21:16:40 -0500

    Starting work on server window.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import SideBar from './sidebar/SideBar';
+import ServerWindow from './server/ServerWindow';
 import './App.css';
 
 class App extends Component {
--- src/server/ServerWindow.js
+++ src/server/ServerWindow.js
@@ -1,12 +1,54 @@
 import React, { Component } from 'react';
 import Server from './Server';
+import net from '../net/net'
 import './ServerWindow.css';
 
 class ServerWindow extends Component {
+    // The actual fetch code happens here
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            servers: [],
+        }
+
+        this.getServers();
+    }
+
+    // renderServer(server) {
+    //     return <Server server={server}/>;
+    // }
+
+    getServers() {
+        fetch(
+            net.endpoint('getAllServers'), // Get the endpoint
+            {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'text/plain' // For CORS reasons
+                },
+            }
+        ).then(res => res.json()) // Handle the Promise
+        .then(res => {
+            // Check error
+            if (res.error) {
+                console.log(res.error);
+                return;
+            }
+
+            // Update the state with the response
+            this.setState({
+                servers: JSON.parse(res.servers)
+            });
+        });
+    }
+
     render() {
         return (
             <div>
-                <Server />
+                <p> {this.state.servers}</p>
+                {/* <Server /> */}
             </div>
         );
     }
